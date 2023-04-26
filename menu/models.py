f422ce65b520ffb545e0e0a6011be407b6f7ca8a
taeyeon0319 <xodus1349@likelion.org>
2023-04-26 16:23:24 +0900

    [김태연]백엔드모델 추가

--- menu/models.py
+++ menu/models.py
@@ -9,21 +9,21 @@ class Menu(models.Model):
         ('음료', '음료'),
 
     )
-    SS_CHOICE = (
-        ('단품', '단품'),
-        ('세트', '세트'),
-    )
     name = models.CharField('메뉴명', max_length=250, unique=True) #메뉴명
     price = models.IntegerField() #메뉴가격
     info = models.TextField() #메뉴설명
     category = models.CharField(max_length=5, choices=CATEGORY) #카테고리
-    ss_choice = models.CharField(max_length=2, choices = SS_CHOICE) #단품세트 선택
     #음식이미지 image = models.ImageField(upload_to="", blank=True, null=True)
 
 '''
 cart 앱 만들고 따로 관리
 class Cart(models.Model):
+    SS_CHOICE = (
+        ('단품', '단품'),
+        ('세트', '세트'),
+    )
     menu = models.ForeignKey(Menu, models.SET_NULL, blank = True, null=True) #장바구니에 넣는 메뉴 
+    ss_choice = models.CharField(max_length=2, choices = SS_CHOICE) #단품세트 선택
     count = models.IntegerField() #메뉴 개수
     
 '''
--- menu/views.py
+++ menu/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def menu(request):
-        return render(request, 'menu/menu_list.html')
+        menus = Menu.objects.all()
+        return render(request, 'menu/menu_list.html', {'menus':menus})
